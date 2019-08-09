9ce7a791bdad2928f17baf6efc3ddf8166dff146
Youxu-Chen <1508303834@qq.com>
2019-08-09 17:30:37 +0800

    fim

--- src/mds/Fim.cc
+++ src/mds/Fim.cc
@@ -47,58 +47,58 @@ Fim::~Fim(){
 /** fim_export_dir - export dir to dest mds
 **/
 void Fim::fim_export_dir(CDir *dir, mds_rank_t dest){
-	fim_dout(7) << __func__ << "export dir " << *dir << " start." << fim_dendl;
+	// fim_dout(7) << __func__ << "export dir " << *dir << " start." << fim_dendl;
 
-	assert(dir->is_auth());
-  	assert(dest != mig->mds->get_nodeid());
+	// assert(dir->is_auth());
+ //  	assert(dest != mig->mds->get_nodeid());
 
-  	if(!(mig->mds->is_active() || mig->mds->is_stopping())){
-  		fim_dout(7) << __func__ << "i'm not active, no exports for now" << fim_dendl;
-  		return;
-  	}
+ //  	if(!(mig->mds->is_active() || mig->mds->is_stopping())){
+ //  		fim_dout(7) << __func__ << "i'm not active, no exports for now" << fim_dendl;
+ //  		return;
+ //  	}
 
-  	if(mig->mds->mdcache->is_readonly()){
-  		fim_dout(7) << __func__ << "read-only FS, no exports for now" << fim_dendl;
-  		return;
-  	}
+ //  	if(mig->mds->mdcache->is_readonly()){
+ //  		fim_dout(7) << __func__ << "read-only FS, no exports for now" << fim_dendl;
+ //  		return;
+ //  	}
 
-  	if(!mig->mds->mdsmap->is_active(dest)){
-  		fim_dout(7) << __func__ << "dest not active, no exports for now" << fim_dendl;
-  		return;
-  	}
+ //  	if(!mig->mds->mdsmap->is_active(dest)){
+ //  		fim_dout(7) << __func__ << "dest not active, no exports for now" << fim_dendl;
+ //  		return;
+ //  	}
 
-  	if(mig->mds->is_cluster_degraded()){
-  		fim_dout(7) << __func__ << "cluster degraded, no exports for now" << fim_dendl;
-  		return;
-  	}
+ //  	if(mig->mds->is_cluster_degraded()){
+ //  		fim_dout(7) << __func__ << "cluster degraded, no exports for now" << fim_dendl;
+ //  		return;
+ //  	}
 
-  	if(dir->inode->is_system()){
-  		fim_dout(7) << __func__ << "i won't export system dirs (root, mdsdirs, stray, /.ceph, etc.)" << fim_dendl;
-  		return;
-  	}
+ //  	if(dir->inode->is_system()){
+ //  		fim_dout(7) << __func__ << "i won't export system dirs (root, mdsdirs, stray, /.ceph, etc.)" << fim_dendl;
+ //  		return;
+ //  	}
 
-  	CDir *parent_dir = dir->inode->get_projected_parent_dir();
-  	if(parent_dir && parent_dir->inode->is_stray()){
-  		if(parent_dir->get_parent_dir()->ino() != MDS_INO_MDSDIR(dest)){
-  			fim_dout(7) << __func__ << "i won't export anything in stray" << fim_dendl;
-  			return;
-  		}
-  	} else{
-  		if(!mig->mds->is_stopping() && !dir->inode->is_exportable(dest)){
-  			fim_dout(7) << __func__ << "dir is export pinned" << fim_dendl;
-  			return;
-  		}
-  	}
+ //  	CDir *parent_dir = dir->inode->get_projected_parent_dir();
+ //  	if(parent_dir && parent_dir->inode->is_stray()){
+ //  		if(parent_dir->get_parent_dir()->ino() != MDS_INO_MDSDIR(dest)){
+ //  			fim_dout(7) << __func__ << "i won't export anything in stray" << fim_dendl;
+ //  			return;
+ //  		}
+ //  	} else{
+ //  		if(!mig->mds->is_stopping() && !dir->inode->is_exportable(dest)){
+ //  			fim_dout(7) << __func__ << "dir is export pinned" << fim_dendl;
+ //  			return;
+ //  		}
+ //  	}
 
-  	if(dir->is_frozen() || dir->is_freezing()){
-  		fim_dout(7) << __func__ << "can't export, freezing|frozen. wait for other exports to finish first." << fim_dendl;
-  		return;
-  	}
+ //  	if(dir->is_frozen() || dir->is_freezing()){
+ //  		fim_dout(7) << __func__ << "can't export, freezing|frozen. wait for other exports to finish first." << fim_dendl;
+ //  		return;
+ //  	}
 
-  	if(dir->state_test(CDir::STATE_EXPORTING)){
-  		fim_dout(7) << __func__ << "already exporting" << fim_dendl;
-  		return;
-  	}
+ //  	if(dir->state_test(CDir::STATE_EXPORTING)){
+ //  		fim_dout(7) << __func__ << "already exporting" << fim_dendl;
+ //  		return;
+ //  	}
 
   	// if(g_conf->mds_thrash_exports){
   	// 	// create random subtree bound (which will not be exported)
@@ -127,20 +127,20 @@ void Fim::fim_export_dir(CDir *dir, mds_rank_t dest){
   	// 	}
   	// }
 
-  	// mig->mds->hit_export_target(ceph_clock_now(), dest, -1);
+  	mig->mds->hit_export_target(ceph_clock_now(), dest, -1);
 
-  	// dir->auth_pin(this);
-  	// dir->state_set(CDir::STATE_EXPORTING);
+  	dir->auth_pin(this);
+  	dir->state_set(CDir::STATE_EXPORTING);
 
-  	// MDRequestRef mdr = mig->mds->mdcache->request_start_internal(CEPH_MDS_OP_EXPORTDIR);
-  	// mdr->more()->export_dir = dir;
+  	MDRequestRef mdr = mig->mds->mdcache->request_start_internal(CEPH_MDS_OP_EXPORTDIR);
+  	mdr->more()->export_dir = dir;
 
-  	// assert(mig->export_state.count(dir) == 0);
-  	// Migrator::export_state_t& stat = mig->export_state[dir];
-  	// stat.state = Migrator::EXPORT_LOCKING;
-  	// stat.peer = dest;
-  	// stat.tid = mdr->reqid.tid;
-  	// stat.mut = mdr;
+  	assert(mig->export_state.count(dir) == 0);
+  	Migrator::export_state_t& stat = mig->export_state[dir];
+  	stat.state = Migrator::EXPORT_LOCKING;
+  	stat.peer = dest;
+  	stat.tid = mdr->reqid.tid;
+  	stat.mut = mdr;
 
-  	// return mig->mds->mdcache->dispatch_request(mdr);
+  	return mig->mds->mdcache->dispatch_request(mdr);
 }
