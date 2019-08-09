c048c6fbb2464d60090f3bcf0a2fd9b6e442be12
Youxu-Chen <1508303834@qq.com>
2019-08-09 17:24:23 +0800

    fim

--- src/mds/Fim.cc
+++ src/mds/Fim.cc
@@ -22,24 +22,19 @@
 
 #include <fstream>
 
-// #define dout_context g_ceph_context
-// #define dout_subsys ceph_subsys_mds_balancer
-// #undef dout_prefix
-// #define dout_prefix *_dout << "mds.fim "
-// #define fim_dout(lvl) \
-//   do {\
-//     auto subsys = ceph_subsys_mds;\
-//     if ((dout_context)->_conf->subsys.should_gather(ceph_subsys_mds_balancer, lvl)) {\
-//       subsys = ceph_subsys_mds_balancer;\
-//     }\
-//     dout_impl(dout_context, subsys, lvl) dout_prefix
-
-// #define fim_dendl dendl; } while (0)
-
 #define dout_context g_ceph_context
-#define dout_subsys ceph_subsys_mds
+#define dout_subsys ceph_subsys_mds_balancer
 #undef dout_prefix
-#define dout_prefix *_dout << "mds." << mds->get_nodeid() << ".fim "
+#define dout_prefix *_dout << "mds.fim "
+#define fim_dout(lvl) \
+  do {\
+    auto subsys = ceph_subsys_mds;\
+    if ((dout_context)->_conf->subsys.should_gather(ceph_subsys_mds_balancer, lvl)) {\
+      subsys = ceph_subsys_mds_balancer;\
+    }\
+    dout_impl(dout_context, subsys, lvl) dout_prefix
+
+#define fim_dendl dendl; } while (0)
 
 Fim::Fim(Migrator *m) : mig(m){
 	fim_dout(7) << " I am Fim, Hi~" << fim_dendl;
@@ -62,90 +57,90 @@ void Fim::fim_export_dir(CDir *dir, mds_rank_t dest){
   		return;
   	}
 
-  	if(mig->mds->mdcache->is_readonly()){
-  		fim_dout(7) << __func__ << "read-only FS, no exports for now" << fim_dendl;
-  		return;
-  	}
+  	// if(mig->mds->mdcache->is_readonly()){
+  	// 	fim_dout(7) << __func__ << "read-only FS, no exports for now" << fim_dendl;
+  	// 	return;
+  	// }
 
-  	if(!mig->mds->mdsmap->is_active(dest)){
-  		fim_dout(7) << __func__ << "dest not active, no exports for now" << fim_dendl;
-  		return;
-  	}
+  	// if(!mig->mds->mdsmap->is_active(dest)){
+  	// 	fim_dout(7) << __func__ << "dest not active, no exports for now" << fim_dendl;
+  	// 	return;
+  	// }
 
-  	if(mig->mds->is_cluster_degraded()){
-  		fim_dout(7) << __func__ << "cluster degraded, no exports for now" << fim_dendl;
-  		return;
-  	}
+  	// if(mig->mds->is_cluster_degraded()){
+  	// 	fim_dout(7) << __func__ << "cluster degraded, no exports for now" << fim_dendl;
+  	// 	return;
+  	// }
 
-  	if(dir->inode->is_system()){
-  		fim_dout(7) << __func__ << "i won't export system dirs (root, mdsdirs, stray, /.ceph, etc.)" << fim_dendl;
-  		return;
-  	}
+  	// if(dir->inode->is_system()){
+  	// 	fim_dout(7) << __func__ << "i won't export system dirs (root, mdsdirs, stray, /.ceph, etc.)" << fim_dendl;
+  	// 	return;
+  	// }
 
-  	CDir *parent_dir = dir->inode->get_projected_parent_dir();
-  	if(parent_dir && parent_dir->inode->is_stray()){
-  		if(parent_dir->get_parent_dir()->ino() != MDS_INO_MDSDIR(dest)){
-  			fim_dout(7) << __func__ << "i won't export anything in stray" << fim_dendl;
-  			return;
-  		}
-  	} else{
-  		if(!mig->mds->is_stopping() && !dir->inode->is_exportable(dest)){
-  			fim_dout(7) << __func__ << "dir is export pinned" << fim_dendl;
-  			return;
-  		}
-  	}
-
-  	if(dir->is_frozen() || dir->is_freezing()){
-  		fim_dout(7) << __func__ << "can't export, freezing|frozen. wait for other exports to finish first." << fim_dendl;
-  		return;
-  	}
-
-  	if(dir->state_test(CDir::STATE_EXPORTING)){
-  		dout(7) << __func__ << "already exporting" << fim_dendl;
-  		return;
-  	}
-
-  	// if(g_conf->mds_thrash_exports){
-  	// 	// create random subtree bound (which will not be exported)
-  	// 	list<CDir*> ls;
-  	// 	for (auto p = dir->begin(); p != dir->end(); ++p){
-  	// 		auto dn = p->second;
-  	// 		CDentry::linkage_t *dnl = dn->get_linkage();
-  	// 		if(dnl->is_primary()){
-  	// 			CInode *in = dnl->get_inode();
-  	// 			if(in->is_dir())
-  	// 				in->get_nested_dirfrags(ls);
-  	// 		}
+  	// CDir *parent_dir = dir->inode->get_projected_parent_dir();
+  	// if(parent_dir && parent_dir->inode->is_stray()){
+  	// 	if(parent_dir->get_parent_dir()->ino() != MDS_INO_MDSDIR(dest)){
+  	// 		fim_dout(7) << __func__ << "i won't export anything in stray" << fim_dendl;
+  	// 		return;
   	// 	}
-
-  	// 	if(ls.size() > 0){
-  	// 		int n = rand() % ls.size();
-  	// 		auto p = ls.begin();
-  	// 		while (n--) ++p;
-  	// 		CDir *bd = *p;
-  	// 		if(!(bd->is_frozen() || bd->is_freezing())){
-  	// 			assert(bd->is_auth());
-  	// 			dir->state_set(CDir::STATE_AUXSUBTREE);
-  	// 			mig->mds->mdcache->adjust_subtree_auth(dir, mig->mds->get_nodeid());
-  	// 			fim_dout(0) << __func__ << "export_dir: create aux subtree" << *bd << " under " << *dir << fim_dendl;
-  	// 		}
+  	// } else{
+  	// 	if(!mig->mds->is_stopping() && !dir->inode->is_exportable(dest)){
+  	// 		fim_dout(7) << __func__ << "dir is export pinned" << fim_dendl;
+  	// 		return;
   	// 	}
   	// }
 
-  	mig->mds->hit_export_target(ceph_clock_now(), dest, -1);
+  	// if(dir->is_frozen() || dir->is_freezing()){
+  	// 	fim_dout(7) << __func__ << "can't export, freezing|frozen. wait for other exports to finish first." << fim_dendl;
+  	// 	return;
+  	// }
 
-  	dir->auth_pin(this);
-  	dir->state_set(CDir::STATE_EXPORTING);
+  	// if(dir->state_test(CDir::STATE_EXPORTING)){
+  	// 	fim_dout(7) << __func__ << "already exporting" << fim_dendl;
+  	// 	return;
+  	// }
 
-  	MDRequestRef mdr = mig->mds->mdcache->request_start_internal(CEPH_MDS_OP_EXPORTDIR);
-  	mdr->more()->export_dir = dir;
+  	// // if(g_conf->mds_thrash_exports){
+  	// // 	// create random subtree bound (which will not be exported)
+  	// // 	list<CDir*> ls;
+  	// // 	for (auto p = dir->begin(); p != dir->end(); ++p){
+  	// // 		auto dn = p->second;
+  	// // 		CDentry::linkage_t *dnl = dn->get_linkage();
+  	// // 		if(dnl->is_primary()){
+  	// // 			CInode *in = dnl->get_inode();
+  	// // 			if(in->is_dir())
+  	// // 				in->get_nested_dirfrags(ls);
+  	// // 		}
+  	// // 	}
 
-  	assert(mig->export_state.count(dir) == 0);
-  	Migrator::export_state_t& stat = mig->export_state[dir];
-  	stat.state = Migrator::EXPORT_LOCKING;
-  	stat.peer = dest;
-  	stat.tid = mdr->reqid.tid;
-  	stat.mut = mdr;
+  	// // 	if(ls.size() > 0){
+  	// // 		int n = rand() % ls.size();
+  	// // 		auto p = ls.begin();
+  	// // 		while (n--) ++p;
+  	// // 		CDir *bd = *p;
+  	// // 		if(!(bd->is_frozen() || bd->is_freezing())){
+  	// // 			assert(bd->is_auth());
+  	// // 			dir->state_set(CDir::STATE_AUXSUBTREE);
+  	// // 			mig->mds->mdcache->adjust_subtree_auth(dir, mig->mds->get_nodeid());
+  	// // 			fim_dout(0) << __func__ << "export_dir: create aux subtree" << *bd << " under " << *dir << fim_dendl;
+  	// // 		}
+  	// // 	}
+  	// // }
 
-  	return mig->mds->mdcache->dispatch_request(mdr);
+  	// mig->mds->hit_export_target(ceph_clock_now(), dest, -1);
+
+  	// dir->auth_pin(this);
+  	// dir->state_set(CDir::STATE_EXPORTING);
+
+  	// MDRequestRef mdr = mig->mds->mdcache->request_start_internal(CEPH_MDS_OP_EXPORTDIR);
+  	// mdr->more()->export_dir = dir;
+
+  	// assert(mig->export_state.count(dir) == 0);
+  	// Migrator::export_state_t& stat = mig->export_state[dir];
+  	// stat.state = Migrator::EXPORT_LOCKING;
+  	// stat.peer = dest;
+  	// stat.tid = mdr->reqid.tid;
+  	// stat.mut = mdr;
+
+  	// return mig->mds->mdcache->dispatch_request(mdr);
 }
