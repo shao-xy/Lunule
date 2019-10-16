6f8d0166dc0b17bc24e8c464d9866770f78afea5
Daniel Shao <sxy799@mail.ustc.edu.cn>
2019-10-16 16:39:14 +0800

    [FIM] First adapt on node 16

--- src/mds/Fim.cc
+++ src/mds/Fim.cc
@@ -1,5 +1,5 @@
-// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
-// vim: ts=8 sw=2 smarttab
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:t -*-
+// vim: ts=4 sw=4 smarttab
 /*
  * Ceph - scalable distributed file system
  *
