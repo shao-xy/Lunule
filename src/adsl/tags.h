c1051dbd9a656b6232f8f01c15b10eea55dc00bc
Daniel Shao <sxy799@mail.ustc.edu.cn>
2019-11-25 15:08:36 +0800

    [TAG] Add string definition of tag ADSLTAG_MIGRATION_CORRE_REQUEST

--- src/adsl/tags.h
+++ src/adsl/tags.h
@@ -1,6 +1,6 @@
 #ifndef _ADSL_TAGS_H_
 #define _ADSL_TAGS_H_
 
-#define ADSLTAG_MIGRATION_CORRE_REQUEST
+#define ADSLTAG_MIGRATION_CORRE_REQUEST "MIGRATION_CORRE_REQUEST"
 
 #endif /* adsl/tags.h */
